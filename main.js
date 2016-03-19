32dacd32ad06b3d64b09211f86daf06af3700d18
Cagdas Yonder <cagdasyonder@gmail.com>
2016-03-18 22:02:41 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,117 @@
+// jshint unused:false
+function toggleHeading(){
+  var ele = document.getElementById("toggleHeading");
+  var text = document.getElementById("displayHeading");
+
+  if(ele.style.display === "block") {
+    ele.style.display = "none";
+    text.innerHTML = "show";
+  }else{
+    ele.style.display = "block";
+    text.innerHTML = "hide";
+  }
+}
+
+function toggleTypography(){
+  var ele = document.getElementById("toggleTypography");
+  var text = document.getElementById("displayTypography");
+
+  if(ele.style.display === "block") {
+    ele.style.display = "none";
+    text.innerHTML = "show";
+  }else{
+    ele.style.display = "block";
+    text.innerHTML = "hide";
+  }
+}
+
+function toggleList(){
+  var ele = document.getElementById("toggleList");
+  var text = document.getElementById("displayList");
+
+  if(ele.style.display === "block") {
+    ele.style.display = "none";
+    text.innerHTML = "show";
+  }else{
+    ele.style.display = "block";
+    text.innerHTML = "hide";
+  }
+}
+
+function toggleButton(){
+  var ele = document.getElementById("toggleButton");
+  var text = document.getElementById("displayButton");
+
+  if(ele.style.display === "block") {
+    ele.style.display = "none";
+    text.innerHTML = "show";
+  }else{
+    ele.style.display = "block";
+    text.innerHTML = "hide";
+  }
+}
+
+function toggleForm(){
+  var ele = document.getElementById("toggleForm");
+  var text = document.getElementById("displayForm");
+
+  if(ele.style.display === "block") {
+    ele.style.display = "none";
+    text.innerHTML = "show";
+  }else{
+    ele.style.display = "block";
+    text.innerHTML = "hide";
+  }
+}
+
+function toggleForm(){
+  var ele = document.getElementById("toggleForm");
+  var text = document.getElementById("displayForm");
+
+  if(ele.style.display === "block") {
+    ele.style.display = "none";
+    text.innerHTML = "show";
+  }else{
+    ele.style.display = "block";
+    text.innerHTML = "hide";
+  }
+}
+
+function toggleTable(){
+  var ele = document.getElementById("toggleTable");
+  var text = document.getElementById("displayTable");
+
+  if(ele.style.display === "block") {
+    ele.style.display = "none";
+    text.innerHTML = "show";
+  }else{
+    ele.style.display = "block";
+    text.innerHTML = "hide";
+  }
+}
+
+function toggleCode(){
+  var ele = document.getElementById("toggleCode");
+  var text = document.getElementById("displayCode");
+
+  if(ele.style.display === "block") {
+    ele.style.display = "none";
+    text.innerHTML = "show";
+  }else{
+    ele.style.display = "block";
+    text.innerHTML = "hide";
+  }
+}
+
+function toggleMisc(){
+  var ele = document.getElementById("toggleMisc");
+  var text = document.getElementById("displayMisc");
+
+  if(ele.style.display === "block") {
+    ele.style.display = "none";
+    text.innerHTML = "show";
+  }else{
+    ele.style.display = "block";
+    text.innerHTML = "hide";
+  }
+}
